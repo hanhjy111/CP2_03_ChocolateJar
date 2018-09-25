b7f71a16917ff2f55c285582523552aa51dd4603
hanhjy111 <hanhjy111@naver.com>
2018-09-26 01:50:10 +0900

    AbstractItem 구현

---
+++ src/data/AbstractItem.java
@@ -0,0 +1,9 @@
+package data;
+
+// 아이템의 추상 클레스
+public abstract class AbstractItem {
+   public abstract String getName();
+
+   static final String CHOCOLATE = "CHOCOLATE";
+   static final String CHILLY = "CHILLY";
+}
